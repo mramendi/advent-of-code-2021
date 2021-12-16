f25410f171889074e1fbe9e64294db55b18d6c4e
Misha Ramendik <mramendi@redhat.com>
2021-12-16 15:51:23 +0000

    Day 16 working

--- 16.py
+++ 16.py
@@ -3,11 +3,9 @@ def parse_packet(packet,depth):
     '''parse a binary packet
     packet is a string representation of binary
     (because I can handle strings better :) )
-    returns a tuple (version,type,content,rest)
+    returns a tuple (version,result,rest)
     version is the version, as an int (also added to version_sum)
-    type is the type, as an int
-    content: if type is 4, then content is the number as an int
-      otherwise content is a list of contents of sub-packets
+    result: the resulting number
     rest: the part iof the input string that remains after one packet was parsed
     '''
     global version_sum
@@ -17,8 +15,8 @@ def parse_packet(packet,depth):
         print("zeros dropped")
         return(0,0,[],"")
 
-    if depth==0:
-        print(packet)
+#    if depth==0:
+#        print(packet)
 
     s_version=packet[0:3]
     s_type=packet[3:6]
@@ -37,42 +35,63 @@ def parse_packet(packet,depth):
             s_rest=s_rest[5:]
             if q=="0":
                 break
-        return (version,type,int(s_literal,2),s_rest)
+        return (version,int(s_literal,2),s_rest)
     else:
+        # first we parse, to find out the content on which to run the operator
+        content=[]
         if s_rest[0]=="0":
             leng=int(s_rest[1:16],2)
             s_rest=s_rest[16:]
             s_subpackets=s_rest[:leng]
             s_rest=s_rest[leng:]
-            print("Length-based start:",leng,"at depth:",depth)
-            content=[]
+#            print("Length-based start:",leng,"at depth:",depth,"remaining bits:",len(s_rest))
             while s_subpackets:
-                print("Length-based ongoing:",len(s_subpackets),"at depth:",depth)
-                t_version,t_type,t_content,s_subpackets=parse_packet(s_subpackets,depth+1)
+#                print("Length-based ongoing:",len(s_subpackets),"at depth:",depth)
+                t_version,t_content,s_subpackets=parse_packet(s_subpackets,depth+1)
                 content.append(t_content)
-            print("Length-based completed at depth:",depth)
-            return (version,type,content,s_rest)
+#            print("Length-based completed at depth:",depth)
         else:
             num=int(s_rest[1:12],2)
-            print("Number-based start:",num,"at depth:",depth)
+#            print("Number-based start:",num,"at depth:",depth)
             s_rest=s_rest[12:]
-            content=[]
             aa=list(range(num))
             for i in aa:
-                print("Number-based:",i,"at depth:",depth)
+#                print("Number-based:",i,"at depth:",depth)
                 #print(s_rest)
-                t_version,t_type,t_content,s_rest=parse_packet(s_rest,depth+1)
+                t_version,t_content,s_rest=parse_packet(s_rest,depth+1)
                 content.append(t_content)
-            return (version,type,content,s_rest)
-
+        # now we handle the content based on the operator
+        result=0 # just to define scope
+        if type==0:
+            result=sum(content)
+        elif type==1:
+            result=1
+            for c in content:
+                result*=c
+        elif type==2:
+            result=min(content)
+        elif type==3:
+            result=max(content)
+        else:
+            if len(content)!=2:
+                print("Content length error")
+            result=0
+            if (type==5) and (content[0]>content[1]):
+                result=1
+            if (type==6) and (content[0]<content[1]):
+                result=1
+            if (type==7) and (content[0]==content[1]):
+                result=1
+        return(version,result,s_rest)
 
 f = open("input16.txt").readlines()
-top_packet=bin(int(f[0],16))[2:].zfill(len(f[0])*4)
-parse_packet(top_packet,0)
-print(version_sum)
+s=f[0].strip()
+top_packet=bin(int(s,16))[2:].zfill(len(s)*4)
 
 # Testing infrastructure for short examples - comment out the above, uncomment this
-#test_str="A0016C880162017C3686B18A3D4780"
+#test_str="880086C3E88112"
 #top_packet=bin(int(test_str,16))[2:].zfill(len(test_str)*4)
-#print (parse_packet(top_packet,0))
-#print(version_sum)
+
+t_version,result,t_rest=parse_packet(top_packet,0)
+print("Part 1:",version_sum)
+print("Part 2:",result)
